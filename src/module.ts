3bfdece826813e9bd5b48001d01650a34d58136e
finleyliang <finleyliang@tencent.com>
2024-06-14 19:24:19 +0800

    feat: refactor types My-variables with CompareQueries prefix

--- src/module.ts
+++ src/module.ts
@@ -2,8 +2,8 @@ import { DataSourcePlugin } from '@grafana/data';
 import { DataSource } from './datasource';
 import { ConfigEditor } from './components/ConfigEditor';
 import { QueryEditor } from './components/QueryEditor';
-import { MyQuery, MyDataSourceOptions } from './types';
+import { CompareQueriesQuery, CompareQueriesOptions } from './types';
 
-export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
+export const plugin = new DataSourcePlugin<DataSource, CompareQueriesQuery, CompareQueriesOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
   .setQueryEditor(QueryEditor);
